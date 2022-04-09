a600f639687f77107c80e21eded4cd5f0290bc83
Olivier R.Measson <olivierberlin1998@hotmail.de>
2022-04-09 15:15:58 +0200

    added random questions

---
+++ js/questions.js
@@ -0,0 +1,37 @@
+var questions = [
+  {
+    title: "JavaScript File Has An Extension of:",
+    choices: [".Java", ".Js", ".javascript", ".lol"],
+    answer: ".Js",
+  },
+  {
+    title: "The Tag is used To Give Heading To The Table.",
+    choices: ["Head", "Td", "Th", "Caption"],
+    answer: "Caption",
+  },
+  {
+    title: "Which Of The Dialog Box Display a Message And a Data Entry Field?",
+    choices: ["Alert()", "Prompt()", "Confirm()", "Msg()"],
+    answer: "Prompt()",
+  },
+  {
+    title: "Event is Used To Check An Empty Text Box:",
+    choices: ["OnClick()", "OnFocus()", "OnBlur()", "None"],
+    answer: "OnBlur()",
+  },
+  {
+    title: "Method Prompt() Contain ........Number of Parameters.",
+    choices: ["One", "Two", "Three", "Zero"],
+    answer: "Two",
+  },
+  {
+    title: "What dose this equation mean ? a != t :",
+    choices: [
+      "A is assigned t",
+      "A and t are equal",
+      "A is not equal to t",
+      "t is added to A",
+    ],
+    answer: "A is not equal to t",
+  },
+];
